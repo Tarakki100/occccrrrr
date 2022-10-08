7814aefb21aed292103e6c4d3e04506e7e9b7668
Mahendra Rajendra Bagul <92087675+Tarakki100@users.noreply.github.com>
2022-10-08 23:26:09 +0530

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -1,18 +1,22 @@
-# command to install easyocr:   pip install easyocr
+#Approach 1: using easyocr
+
+# command to install easyocr:   
+#pip install easyocr
 
 
 
 import easyocr
 
-image = 'SIGNA.jpg'
+image = 'SIGNA.jpg'     #read image
 
-read = easyocr.Reader(['en'])
-result = read.readtext(image,paragraph="False")
-print(result)
+read = easyocr.Reader(['en'])      #take only english language content
+result = read.readtext(image,paragraph="False")            #read the text present in english language
+print(result)   #print the text 
 
 
+#Approach 2: using pytesseract
 
-#command to install pytessract
+#command to install pytessract and opencv python
 #pip install pytesseract
 #pip install opencv-python
 
@@ -20,6 +24,6 @@ print(result)
 import pytesseract
 import cv2
 
-img = cv2.imread('dummy.jpg')
-result = pytesseract.image_to_string(img)
-print(result)
+img = cv2.imread('dummy.jpg')    #read image
+result = pytesseract.image_to_string(img)    #capture the text
+print(result)     #print the text
